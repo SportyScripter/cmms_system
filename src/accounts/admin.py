ff262e23c98a8330052093a112a84222cde4b7b2
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2026-03-24 00:54:54 +0100

    feat: implement custom user model with RBAC roles

---
+++ src/accounts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# Register your models here.
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (("Uprawnienia CMMS", {"fields": ("role",)}),)
+    list_display = ("username", "email", "first_name", "last_name", "role", "is_staff")
+    list_filter = ("role", "is_staff", "is_active")
