1710b1d317357f331448d97f2210d938bf815dee
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2026-03-29 20:57:16 +0200

    feat: implement JWT authentication to secure the REST API

--- src/config/urls.py
+++ src/config/urls.py
@@ -22,11 +22,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from warehouse.views import PartViewSet
 
+# --- JWT Authentication Endpoints ---
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r"parts", PartViewSet)
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include(router.urls))]
+urlpatterns = [path("admin/", admin.site.urls),
+               path("api/token/",TokenObtainPairView.as_view(),name="token_obtain_pair"),
+               path("api/token/refresh/",TokenRefreshView.as_view(),name="token_refresh"),
+               path("api/", include(router.urls))]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
+
