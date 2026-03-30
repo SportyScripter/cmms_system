1d687b4d69af4c2828d24b09f5e82cb749844a11
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2026-03-30 22:54:42 +0200

    feat: complete REST API by exposing users and inventory logs

--- src/warehouse/serializers.py
+++ src/warehouse/serializers.py
@@ -1,12 +1,46 @@
 from rest_framework import serializers
-from .models import Part
+from .models import Part, InventoryLog
+
 
 class PartSerializer(serializers.ModelSerializer):
     machine_list = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Part
-        fields = ['id','name','unique_code','quantity','min_quantity','location','price','machine_list','barecode_image']
-    
+        fields = [
+            "id",
+            "name",
+            "unique_code",
+            "quantity",
+            "min_quantity",
+            "location",
+            "price",
+            "machine_list",
+            "barecode_image",
+        ]
+
     def get_machine_list(self, obj):
         return [machine.name for machine in obj.machines.all()]
+
+
+class InventoryLogSerializer(serializers.ModelSerializer):
+    part_name = serializers.CharField(source="part.name", read_only=True)
+    user_name = serializers.CharField(source="user.username", read_only=True)
+    work_order_title = serializers.CharField(source="work_order.title", read_only=True)
+
+    class Meta:
+        model = InventoryLog
+        fields = [
+            "id",
+            "part",
+            "part_name",
+            "user",
+            "user_name",
+            "work_order",
+            "work_order_title",
+            "transaction_type",
+            "quantity",
+            "timestamp",
+            "notes",
+        ]
+        read_only_fields = ["timestamp"]
--- src/warehouse/views.py
+++ src/warehouse/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Part
-from .serializers import PartSerializer
+from .models import Part , InventoryLog
+from .serializers import PartSerializer, InventoryLogSerializer
 
 
 class PartViewSet(viewsets.ModelViewSet):
     queryset = Part.objects.all()
     serializer_class = PartSerializer
+
+class InventoryLogViewSet(viewsets.ModelViewSet):
+    queryset = InventoryLog.objects.all()
+    serializer_class = InventoryLogSerializer
